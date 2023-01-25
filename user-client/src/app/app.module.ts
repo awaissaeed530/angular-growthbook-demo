40553406132efbce969380c6e166c5d309416f75
Awais Saeed <awaissaeed530@gmail.com>
2023-01-26 03:19:45 +0500

    add router

--- user-client/src/app/app.module.ts
+++ user-client/src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { environment } from 'src/environments';
 import { AppComponent } from './app.component';
-import { GrowthBookModule } from './core/growth-book';
+import { FeaturesResolver, GrowthBookModule } from './core';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    resolve: { features: FeaturesResolver },
+    children: [{ path: '', component: HomeComponent }],
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     CommonModule,
     BrowserModule,
+    RouterModule.forRoot(routes),
+
     GrowthBookModule.forRoot({
       apiHost: 'http://localhost:3100',
       clientKey: 'sdk-sohtXJJ0iCeYyFW',
---
+++ user-client/src/app/home.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `<p *feature="'home-v2'">Home V2</p>`,
+})
+export class HomeComponent {}
